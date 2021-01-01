fda0c9b759cc1275333c22d91a9e80242697cdc4
jvrs90 <jvrs.90@gmail.com>
2021-01-01 12:39:19 +0100

    Follows

--- packages/nevook/src/components/profile/sections/profile-follow.tsx
+++ packages/nevook/src/components/profile/sections/profile-follow.tsx
@@ -1,7 +1,48 @@
 import { FC } from 'react';
 
 const ProfileFollow: FC = () => {
-	return <p>Seguidores y más cosas</p>;
+  return (
+    <>
+      <UserFollowList />
+    </>
+  );
+};
+
+const UserFollowList = () => {
+  return (
+    <>
+      <UserFollowItem isFollowing={true} />
+      <UserFollowItem isFollowing={false} />
+      <UserFollowItem isFollowing={true} />
+      <UserFollowItem isFollowing={true} />
+      <UserFollowItem isFollowing={false} />
+    </>
+  );
+};
+
+// TODO: Factorizar estos dos componentes
+const UserFollowItem = ({ isFollowing }) => {
+  return (
+    <>
+      <div className='UserFollowItem flex justify-between my-1'>
+        <div className="flex">
+          <img
+            className='rounded-full'
+            src='https://via.placeholder.com/70x70'
+            alt='Foto perfil'
+          />
+          <div className='pl-0_5'>
+            <h1>Jose</h1>
+            <p>Descripcion del usuario</p>
+          </div>
+        </div>
+
+        <button className='bg-cta p-0_5 text-white rounded'>
+          {isFollowing ? 'Siguiendo' : 'Seguir'}
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default ProfileFollow;
